0ddfe0571530cd29e76e81ddd930ac4a50b922ee
Nir Natan <nirn@wix.com>
2018-05-16 19:41:43 +0300

    fix bug in values() function

--- src/templates/optimizing.js
+++ src/templates/optimizing.js
@@ -473,6 +473,7 @@ function base() {
             $deletedKeys.push(key);
           } else {
             if ($keyToIdx.hasOwnProperty(key)) {
+              $out[$keyToIdx[key]] = src[key];
               triggerInvalidations($out, $keyToIdx[key]);
             }
           }
