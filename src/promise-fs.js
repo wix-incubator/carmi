4720ec40f830d075492efa5725d58a78a74c816a
Gal Schlezinger <gal@spitfire.co.il>
2018-10-28 14:57:19 +0200

    Use `pify` for promisifying things (#10)

--- src/promise-fs.js
+++ src/promise-fs.js
@@ -1,20 +1,3 @@
-const fs = require('fs');
-
-module.exports = ['readFile', 'writeFile', 'mkdtemp', 'rmdir', 'unlink'].reduce((acc, func, key) => {
-  acc[key] = (...args) => {
-    return new Promise((resolve, reject) => {
-      func.apply(
-        fs,
-        args.concat([
-          (err, result) => {
-            if (err) {
-              reject(err);
-            }
-            resolve(result);
-          }
-        ])
-      );
-    });
-  };
-  return acc;
-}, {});
+const fs = require("fs");
+const pify = require("pify");
+module.exports = pify(fs);
