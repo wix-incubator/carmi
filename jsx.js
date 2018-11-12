5443d7b426b4bdbebbcfbdfbd2f0284c7693e57d
Avi Marcus <avim@wix.com>
2018-11-12 10:11:02 +0200

    changed jsx to be a simple array

--- jsx.js
+++ jsx.js
@@ -1,14 +1,7 @@
 const { chain } = require('./index');
 
-function createElement(type, props, ...children) {
-  if (children.length) {
-    return chain({ type, props, children }).call('createElement');
-  }
-  return chain({ type, props }).call('createElement');
+function createElement(...args) {
+  return chain([...args]).call('createElement');
 }
 
-function bind(funcName, ...args) {
-  return chain([funcName, ...args]).call('bind');
-}
-
-module.exports = { createElement, bind };
+module.exports = { createElement };
