c11fc3b9e032ce921fa532e555be5bb32644330c
reutsa <reutsa@wix.com>
2018-11-13 15:17:20 +0200

    change getIn to support lodash syntax (expects a path array)

--- src/sugar.js
+++ src/sugar.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-function getIn(obj, ...path) {
+function getIn(obj, path) {
     return _.reduce(path, (acc, val) => {
       return acc.ternary(acc.get(val), acc)
     }, obj);
