d475ccbcfaf8ff56d48b1b7c0377eacc44a1a3a5
Or Granit <orgr@wix.com>
2018-12-04 00:39:22 +0200

    Sugar: implemented set and push

--- src/sugar.js
+++ src/sugar.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-module.exports = function(chain) {
+module.exports = function({chain, or, and}) {
     function getIn(obj, path) {
         return _.reduce(path, (acc, val) => {
             return acc.ternary(acc.get(val), acc)
@@ -31,18 +31,45 @@ module.exports = function(chain) {
         return a.size().plus(b.size()).range().map(v => v.lt(a.size()).ternary(a.get(v), b.get(v.minus(a.size()))))
     }
 
-  function find(collection, predicate, ctx) {
-    return collection.values().filter((val, key) => predicate(val, key, ctx)).get(0)
-  }
+    function find(collection, predicate, ctx) {
+      return collection.values().filter((val, key) => predicate(val, key, ctx)).get(0)
+    }
 
-  function join(arr, seperator) {
-    return reduce(arr, (acc, value, index) => index.eq(0).ternary(acc.plus(value), acc.plus(seperator).plus(value)), '')
-  }
+    function join(arr, seperator) {
+      return reduce(arr, (acc, value, index) => index.eq(0).ternary(acc.plus(value), acc.plus(seperator).plus(value)), '')
+    }
 
-  function sum(arr) {
-      return reduce(arr, (acc, value) => acc.plus(value), 0)
-  }
+    function sum(arr) {
+        return reduce(arr, (acc, value) => acc.plus(value), 0)
+    }
 
-    return { getIn, includes, assignIn, reduce, concat, find, join, sum };
+    function append(arr, value) {
+      return arr.size().plus(1).range().map(v => v.lt(arr.size()).ternary(arr.get(v), value))
+    }
+
+    function setIn(obj, path, value) {
+      if (!Array.isArray(path) || path.length === 0) {
+          throw new Error('only set with array paths');
+      }
+      path.forEach(val => {
+          if (typeof val !== 'string') {
+              throw new Error('all path parts in set should be strings');
+          }
+      })
+
+      function step(base, part, value) {
+          return chain([base, {[part]: value}]).assign()
+      }
+
+      const currentValues = path.map((part, index) => 
+        or(getIn(obj, path.slice(0, index)), chain({}))
+      )
+
+      return path.reduceRight((acc, part, index) => {
+          return step(currentValues[index], part, acc)
+      }, value)
+    }
+
+    return { getIn, includes, assignIn, reduce, concat, find, join, sum, append, setIn };
 };
 
