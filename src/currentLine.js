a34d5fc6a19a3db97b2be7dfdf1ebf1f368ad03a
michabeeri <michaelb@wix.com>
2019-03-03 17:56:11 +0200

    refactor currentLine.js

--- src/currentLine.js
+++ src/currentLine.js
@@ -1,22 +1,29 @@
-const _ = require('lodash');
-const path = require('path');
-const ignoredLinePatterns =
-  [
-    'sugar.js',
-    'expressionBuilder.js',
-    'frontend.js',
-    'proxyHandler.js',
-    'currentLine'
-  ]
-  .map(relative => path.resolve(__dirname, relative))
-  .concat([path.resolve(__dirname, '../index.js'), path.resolve(__dirname, '../jsx.js')]);
+'use strict'
+
+const _ = require('lodash')
+const path = require('path')
+const carmiRoot = require('../carmiRoot')
+const testPaths = [
+  '__tests__',
+  'babelPlugin'
+].map(p => path.resolve(__dirname, p))
+
+function isExternalLine(line) {
+  const isCarmi = _.includes(line, carmiRoot)
+  const isTest = _.some(testPaths, p => _.includes(line, p))
+  const containsLineNumber = _.includes(line, ':')
+  return (!isCarmi || isTest) && containsLineNumber
+}
 
 module.exports = () => {
-  const e = new Error();
-  const lines = e.stack.split('\n');
-  const externalLine =
-    lines
-      .slice(1)
-      .filter(l => !_.some(ignoredLinePatterns, p => l.indexOf(p) !== -1) && l.indexOf(':') !== -1)[0] || 'unknown';
-  return externalLine.substr(externalLine.indexOf(path.sep)).split(':').map((str, idx) => idx > 0 ? `${parseInt(str, 10)}` : str).join(':')
+  const firstExternalLine = (new Error()).stack
+    .split('\n')
+    .slice(1)
+    .find(isExternalLine) || 'unknown'
+
+  return firstExternalLine
+    .substr(firstExternalLine.indexOf(path.sep))
+    .split(':')
+    .map((str, idx) => idx > 0 ? `${parseInt(str, 10)}` : str)
+    .join(':')
 }
