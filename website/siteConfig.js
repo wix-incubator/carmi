b24120f5852d50f25e60ce9077814210d0c629ba
LeonFedotov <leonf@wix.com>
2019-03-20 11:59:33 +0200

    adding edit links to all the markdowns plus the one thats generated with typings

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,6 +24,7 @@ const siteConfig = {
   title: 'CARMI' /* title for your website */,
   tagline: 'Compiler for Automatic Reactive Modelling of Inference',
   url: 'https://carmi.js.org' /* your website url */,
+  editUrl: 'https://github.com/wix-incubator/carmi/edit/master/docs/',
   baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
