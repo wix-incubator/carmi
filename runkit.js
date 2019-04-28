502b82581c349974002f667755bf31e5bcdc4a14
Leon Fedotov <fedotov.leon@gmail.com>
2019-04-28 15:18:12 +0300

    Deploy website
    Deploy website version based on 07a313cc4a3e52253cbb514ffc908abf7e573ce1

--- runkit.js
+++ runkit.js
@@ -1,22 +1,51 @@
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('remove')) {
+      return;
+    }
+    Object.defineProperty(item, 'remove', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function remove() {
+        this.parentNode.removeChild(this);
+      }
+    });
+  });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
 window.addEventListener('load', function() {
   const preamble = 'const createInstance = (model, input = [1, 2, 3], lib = {}) => eval(require("carmi").compile(model))(input, lib)'
   const script = document.createElement('script')
-  script.addEventListener('load', () => Array.from(document.getElementsByClassName('hljs')).map(element => {
-    element.style.cursor = 'pointer'
-    element.addEventListener('click', () => {
-      const source = element.innerText
-      element.style.display = 'none'
-      element.parentNode.style.background = 'url(/img/loading.gif) no-repeat center'
-      RunKit.createNotebook({
-        element: element.parentNode,
-        preamble,
-        source
-      })
+  const run = (play) => ({target}) => {
+    const element = target.previousSibling
+    const source = element.innerText
+    element.style.display = 'none'
+    element.parentNode.style.background = 'url(/img/loading.gif) no-repeat center'
+    RunKit.createNotebook({
+      element: element.parentNode,
+      preamble,
+      source,
+      onLoad: () => clean(play)
     })
+  }
+
+  const clean = (e) => {
+    e.removeEventListener('click', run)
+    e.parentElement.style.background = 'initial'
+    e.remove()
+  }
+
+  script.addEventListener('load', () => Array.from(document.getElementsByClassName('hljs')).map(element => {
+    const play = document.createElement('span')
+    play.innerText = '▶'
+    play.className = 'edit-page-link button interactive-action'
+    play.addEventListener('click', run(play))
+    element.parentElement.appendChild(play)
   }))
   script.src = 'https://embed.runkit.com'
   script.async = true
   document.getElementsByTagName('script')[0].parentNode.appendChild(script);
 
 })
-
