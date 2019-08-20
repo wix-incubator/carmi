8917897baadc84c70175e30ad7f92a5528bc59ff
Artem Yavorsky <aqson@me.com>
2019-08-20 14:41:37 +0300

    Fix plugin tester code generation

--- src/babelPlugin/macro-debug.spec.js
+++ src/babelPlugin/macro-debug.spec.js
@@ -2,44 +2,43 @@ const pluginTester = require('babel-plugin-tester');
 const plugin = require('babel-plugin-macros');
 const path = require('path');
 const babel = require('babel-core');
-const MACROS_CONFIG = path.join(process.cwd(), 'babel-plugin-macros.config.js');
 
-describe('Macro with debug option', () => {
-  jest.mock(MACROS_CONFIG, () => ({carmi: {debug: true}}))
+jest.mock('../../babel-plugin-macros.config.js', () => ({carmi: {debug: true}}))
 
-  pluginTester({
-    plugin,
-    snapshot: true,
-    babelOptions: {
-      filename: path.resolve(__dirname, 'temp.js'),
-      presets: ['react']
-    },
-    tests: {
-      'template-literal': `
-        const carmi = require('./macro')
-  
-        const modelBuilder = carmi\`
-          const {root} = require('../..')
-          module.exports = {all: root.get('list'), first: root.get('list').get(0)}
-        \`
-      `,
-      'magic-comment': `
+pluginTester({
+  plugin,
+  snapshot: true,
+  babelOptions: {
+    filename: path.resolve(__dirname, 'temp.js'),
+    presets: ['react']
+  },
+  tests: {
+    'template-literal': `
+      const carmi = require('./macro')
+
+      const modelBuilder = carmi\`
+        const {root} = require('../..')
+        module.exports = {all: root.get('list'), first: root.get('list').get(0)}
+      \`
+    `,
+    'magic-comment': `
+  // @carmi
+  import carmi from './macro'
+  const { root } = require('../../index');
+  module.exports = { first: root.get(0), second: root.get(1) };
+`,
+    'carmi-react': `
     // @carmi
     import carmi from './macro'
     const { root } = require('../../index');
-    module.exports = { first: root.get(0), second: root.get(1) };
-  `,
-      'carmi-react': `
-      // @carmi
-      import carmi from './macro'
-      const { root } = require('../../index');
-      const {createElement} = require('../../jsx');
-      const todosList = <div>{root.get(0)}</div>;
-      module.exports = {todosList};
-  `
-    }
-  });
+    const {createElement} = require('../../jsx');
+    const todosList = <div>{root.get(0)}</div>;
+    module.exports = {todosList};
+`
+  }
+});
 
+describe('Macro with debug option', () => {
   it('works with debug mode', done => {
     const code = `
       const carmi = require('./macro')
